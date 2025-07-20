01d0a1c9c43a8bc33b91fbfe2a89dfa557932f55
Geleta Tamiru <geletatamiru@yahoo.com>
2025-07-20 15:25:43 +0300

    made some changes.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import { useAuth } from "./context/AuthContext";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import ChatPage from "./pages/Chatpage";
+import ChatPage from "./pages/ChatPage";
 import "./App.css";
 function App() {
   const { token } = useAuth();
