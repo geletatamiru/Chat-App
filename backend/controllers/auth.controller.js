3e431c1ce6df95e093cfd3961f983822ad8866ca
geletatamiru <150704342+geletatamiru@users.noreply.github.com>
2025-12-04 14:11:50 +0300

    Update cookies options

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -108,8 +108,8 @@ const login = async (req, res) => {
 
   res.cookie('refreshToken', refreshToken, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    secure: true,
+    sameSite: "none",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   }).json({
     success: true,
